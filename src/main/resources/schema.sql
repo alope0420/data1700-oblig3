c28e99d2a655f95e16538db3835f84ec693bbf22
alope0420 <alexander@opedal.org>
2024-04-01 02:42:55 +0200

    add h2/jdbc/jpa dependencies, configure data source, add basic schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create table
+    tickets (
+        movie text not null,
+        count tinyint not null,
+        firstname text not null,
+        lastname text not null,
+        tel text not null,
+        email text not null
+    );
